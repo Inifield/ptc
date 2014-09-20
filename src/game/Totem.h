40c0ddb19e9ce1512592c8d4faae8d23879c9864
Zaffy <zaffy@lunar.sk>
2014-09-20 22:26:07 +0200

    Fix previous commit

---
+++ src/game/Totem.h
@@ -0,0 +1,60 @@
+/*
+ * This file is part of the OregonCore Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef OREGONCORE_TOTEM_H
+#define OREGONCORE_TOTEM_H
+
+#include "TemporarySummon.h"
+
+enum TotemType
+{
+    TOTEM_PASSIVE    = 0,
+    TOTEM_ACTIVE     = 1,
+};
+
+#define SENTRY_TOTEM_ENTRY  3968
+
+class Totem : public Minion
+{
+    public:
+        explicit Totem(SummonPropertiesEntry const *properties, Unit *owner);
+        virtual ~Totem(){};
+        void Update(uint32 time);
+        void InitStats(uint32 duration);
+        void InitSummon();
+        void UnSummon();
+        uint32 GetSpell() const { return m_spells[0]; }
+        uint32 GetTotemDuration() const { return m_duration; }
+        TotemType GetTotemType() const { return m_type; }
+
+        bool UpdateStats(Stats /*stat*/) { return true; }
+        bool UpdateAllStats() { return true; }
+        void UpdateResistances(uint32 /*school*/) {}
+        void UpdateArmor() {}
+        void UpdateMaxHealth() {}
+        void UpdateMaxPower(Powers /*power*/) {}
+        void UpdateAttackPowerAndDamage(bool /*ranged*/) {}
+        void UpdateDamagePhysical(WeaponAttackType /*attType*/) {}
+
+        bool IsImmuneToSpellEffect(SpellEntry const* spellInfo, uint32 index, bool castOnSelf) const;
+
+    protected:
+        TotemType m_type;
+        uint32 m_duration;
+};
+#endif
+
