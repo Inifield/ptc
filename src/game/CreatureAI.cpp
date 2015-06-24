272d23bbf453f9496ec4d7dd85ee409f0bc21442
Zaffy <zaffy@lunar.sk>
2015-06-24 15:07:14 +0200

    Fix #1018 - singleton crash
    * remove singletonimp.h, move it to singleton.h (this solves the crash),
    it was probably to some implmentation-undefined behavior with transaltion units
    * remove singleton sThreadList and integrate it directly to UnixDebugger class,
    and make it protected by a mutex, this way it's thread-safe
    * also a small change to wheatyexception, oc will stop if it freezes, making it
    possible to debug it better

---
+++ src/game/CreatureAI.cpp
@@ -0,0 +1,299 @@
+/*
+ * This file is part of the OregonCore Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "CreatureAI.h"
+#include "CreatureAIImpl.h"
+#include "Creature.h"
+#include "World.h"
+#include "SpellMgr.h"
+#include "MapRefManager.h"
+#include "Player.h"
+
+//Disable CreatureAI when charmed
+void CreatureAI::OnCharmed(bool apply)
+{
+    me->NeedChangeAI = true;
+
+    if(!(me->ToCreature()->GetCreatureTemplate()->flags_extra & CREATURE_FLAG_EXTRA_CHARM_AI))
+        me->IsAIEnabled = !apply;
+}
+
+AISpellInfoType* UnitAI::AISpellInfo;
+AISpellInfoType* GetAISpellInfo(uint32 i)
+{
+    return &CreatureAI::AISpellInfo[i];
+}
+
+void CreatureAI::DoZoneInCombat(Creature* creature /*= NULL*/, float maxRangeToNearestTarget /* = 50.0f*/)
+{
+    if (!creature)
+        creature = me;
+
+    if (!creature->CanHaveThreatList())
+        return;
+
+    Map* map = creature->GetMap();
+    if (!map->IsDungeon())                                  //use IsDungeon instead of Instanceable, in case battlegrounds will be instantiated
+    {
+        sLog.outError("DoZoneInCombat call for map that isn't an instance (creature entry = %d)", creature->GetTypeId() == TYPEID_UNIT ? creature->ToCreature()->GetEntry() : 0);
+        return;
+    }
+
+    if (!creature->HasReactState(REACT_PASSIVE) && !creature->getVictim())
+    {
+        if (Unit* nearTarget = creature->SelectNearestTarget(maxRangeToNearestTarget))
+            creature->AI()->AttackStart(nearTarget);
+        else if (creature->IsSummon())
+        {
+            if (Unit* summoner = ((TempSummon*)creature)->GetSummoner())
+            {
+                Unit* target = summoner->getAttackerForHelper();
+                if (!target && summoner->CanHaveThreatList() && !summoner->getThreatManager().isThreatListEmpty())
+                    target = summoner->getThreatManager().getHostileTarget();
+                if (target && (creature->IsFriendlyTo(summoner) || creature->IsHostileTo(target)))
+                    creature->AI()->AttackStart(target);
+            }
+        }
+    }
+
+    if (!creature->HasReactState(REACT_PASSIVE) && !creature->getVictim())
+    {
+        sLog.outError("DoZoneInCombat called for creature that has empty threat list (creature entry = %u)", creature->GetEntry());
+        return;
+    }
+
+    Map::PlayerList const& PlList = map->GetPlayers();
+
+    if (PlList.isEmpty())
+        return;
+
+    for (Map::PlayerList::const_iterator i = PlList.begin(); i != PlList.end(); ++i)
+    {
+        if (Player* pPlayer = i->getSource())
+        {
+            if (pPlayer->isGameMaster())
+                continue;
+
+            if (pPlayer->IsAlive())
+            {
+                creature->SetInCombatWith(pPlayer);
+                pPlayer->SetInCombatWith(creature);
+                creature->AddThreat(pPlayer, 0.0f);
+            }
+
+            /* Causes certain things to never leave the threat list (Priest Lightwell, etc):
+            for (Unit::ControlList::const_iterator itr = pPlayer->m_Controlled.begin(); itr != pPlayer->m_Controlled.end(); ++itr)
+            {
+                creature->SetInCombatWith(*itr);
+                (*itr)->SetInCombatWith(creature);
+                creature->AddThreat(*itr, 0.0f);
+            }*/
+        }
+    }
+}
+
+// scripts does not take care about MoveInLineOfSight loops
+// MoveInLineOfSight can be called inside another MoveInLineOfSight and cause stack overflow
+void CreatureAI::MoveInLineOfSight_Safe(Unit* who)
+{
+    if (m_MoveInLineOfSight_locked == true)
+        return;
+    m_MoveInLineOfSight_locked = true;
+    MoveInLineOfSight(who);
+    m_MoveInLineOfSight_locked = false;
+}
+
+void CreatureAI::MoveInLineOfSight(Unit* who)
+{
+    if (me->getVictim())
+        return;
+    
+    if (me->GetCreatureType() == CREATURE_TYPE_NON_COMBAT_PET) // non-combat pets should just stand there and look good;)
+        return;
+
+    if (me->canStartAttack(who))
+        AttackStart(who);
+
+    //else if (who->getVictim() && me->IsFriendlyTo(who)
+    //    && me->IsWithinDistInMap(who, sWorld.getConfig(CONFIG_CREATURE_FAMILY_ASSISTANCE_RADIUS))
+    //    && me->canAttack(who->getVictim()))
+    //    AttackStart(who->getVictim());
+}
+
+bool CreatureAI::UpdateVictimByReact()
+{
+    if (!me->IsInCombat())
+        return false;
+
+    if (me->HasReactState(REACT_AGGRESSIVE))
+    {
+        if (Unit* victim = me->SelectVictim())
+            AttackStart(victim);
+        return me->getVictim();
+    }
+    else if (me->getThreatManager().isThreatListEmpty())
+    {
+        EnterEvadeMode();
+        return false;
+    }
+
+    return true;
+}
+
+void CreatureAI::EnterEvadeMode()
+{
+    if (!_EnterEvadeMode())
+        return;
+
+    sLog.outDebug("Creature %u enters evade mode.", me->GetEntry());
+
+    if (Unit* owner = me->GetCharmerOrOwner())
+    {
+        me->GetMotionMaster()->Clear(false);
+        me->GetMotionMaster()->MoveFollow(owner, PET_FOLLOW_DIST, me->GetFollowAngle(), MOTION_SLOT_ACTIVE);
+    }
+    else
+    {
+        // Required to prevent attacking creatures that are evading and cause them to reenter combat
+        // Does not apply to MoveFollow
+        me->AddUnitState(UNIT_STATE_EVADE);
+        me->GetMotionMaster()->MoveTargetedHome();
+    }
+
+    Reset();
+}
+
+/*void CreatureAI::AttackedBy(Unit* attacker)
+{
+    if (!me->getVictim())
+        AttackStart(attacker);
+}*/
+
+
+void CreatureAI::SetGazeOn(Unit* target)
+{
+    if (me->canAttack(target))
+    {
+        AttackStart(target);
+        me->SetReactState(REACT_PASSIVE);
+    }
+}
+
+bool CreatureAI::UpdateVictimWithGaze()
+{
+    if (!me->IsInCombat())
+        return false;
+
+    if (me->HasReactState(REACT_PASSIVE))
+    {
+        if (me->getVictim())
+            return true;
+        else
+            me->SetReactState(REACT_AGGRESSIVE);
+    }
+
+    if (Unit* victim = me->SelectVictim())
+        AttackStart(victim);
+    return me->getVictim();
+}
+
+bool CreatureAI::UpdateVictim()
+{
+    if (!me->IsInCombat())
+        return false;
+
+    if (!me->HasReactState(REACT_PASSIVE))
+    {
+        if (Unit* victim = me->SelectVictim())
+            AttackStart(victim);
+        return me->getVictim();
+    }
+    else if (me->getThreatManager().isThreatListEmpty())
+    {
+        EnterEvadeMode();
+        return false;
+    }
+
+    return true;
+}
+
+bool CreatureAI::_EnterEvadeMode()
+{
+    if (!me->IsAlive())
+        return false;
+
+    // sometimes bosses stuck in combat?
+    me->DeleteThreatList();
+    me->CombatStop(true);
+    me->SetLootRecipient(NULL);
+    me->ResetPlayerDamageReq();
+    me->SetLastDamagedTime(0);
+
+    if (me->IsInEvadeMode())
+        return false;
+
+    me->RemoveAllAuras();
+    me->LoadCreaturesAddon();
+    // @todo Determine if required
+    //me->SetReactState(REACT_AGGRESSIVE);
+
+    return true;
+}
+
+void CreatureAI::SetCombatMovement(bool enable)
+{
+    if (CombatMovementEnabled == enable)
+        return;
+
+    CombatMovementEnabled = enable;
+
+    if (enable && me->getVictim())
+    {
+        if (me->GetMotionMaster()->GetCurrentMovementGeneratorType() == IDLE_MOTION_TYPE || ASSISTANCE_MOTION_TYPE)
+        {
+            me->GetMotionMaster()->MoveChase(me->getVictim());
+            me->CastStop();
+        }
+    }
+    else
+    {
+        me->GetMotionMaster()->MovementExpired();
+        me->GetMotionMaster()->Clear(true);
+        me->StopMoving();
+        me->GetMotionMaster()->MoveIdle();
+    }
+}
+
+Creature* CreatureAI::DoSummon(uint32 entry, const Position& pos, uint32 despawnTime, TempSummonType summonType)
+{
+    return me->SummonCreature(entry, pos, summonType, despawnTime);
+}
+
+Creature* CreatureAI::DoSummon(uint32 entry, WorldObject* obj, float radius, uint32 despawnTime, TempSummonType summonType)
+{
+    Position pos;
+    obj->GetRandomNearPosition(pos, radius);
+    return me->SummonCreature(entry, pos, summonType, despawnTime);
+}
+
+Creature* CreatureAI::DoSummonFlyer(uint32 entry, WorldObject* obj, float flightZ, float radius, uint32 despawnTime, TempSummonType summonType)
+{
+    Position pos;
+    obj->GetRandomNearPosition(pos, radius);
+    pos.m_positionZ += flightZ;
+    return me->SummonCreature(entry, pos, summonType, despawnTime);
+}
