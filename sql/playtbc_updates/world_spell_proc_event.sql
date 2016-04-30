f9440ac6eec8123cfdae46d7004c1492b92d6d07
xpearlis <xpearlis@localhost>
2016-04-30 23:48:46 +0200

    [MAGE] Talent Master of Elements(29076) not working properly

---
+++ sql/playtbc_updates/world_spell_proc_event.sql
@@ -0,0 +1,3 @@
+UPDATE `spell_proc_event` SET `Cooldown` = '1' WHERE `spell_proc_event`.`entry` = 29074; 
+UPDATE `spell_proc_event` SET `Cooldown` = '1' WHERE `spell_proc_event`.`entry` = 29075; 
+UPDATE `spell_proc_event` SET `Cooldown` = '1' WHERE `spell_proc_event`.`entry` = 29076; 
